82bb8cba3c8e66a3b74f5102bee7208324d452cc
mominabaig <mominaasif95@gmail.com>
2024-01-27 19:14:22 -0800

    Initial commit with door class implementation

---
+++ door.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Door {
+private:
+    bool isOpen;
+
+public:
+    Door() : isOpen(false) {}
+
+    void open() {
+        isOpen = true;
+        cout << "Door is open." << endl;
+    }
+
+    void close() {
+        isOpen = false;
+        cout << "Door is closed." << endl;
+    }
+
+    bool isOpenStatus() {
+        return isOpen;
+    }
+};
+
+int main() {
+    Door frontDoor;
+    frontDoor.open();
+    cout << "Is the door open? " << (frontDoor.isOpenStatus() ? "Yes" : "No") << endl;
+    frontDoor.close();
+    cout << "Is the door open? " << (frontDoor.isOpenStatus() ? "Yes" : "No") << endl;
+
+    return 0;
+}
